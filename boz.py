546728f7b92ca14f015e2c27f4a065f49bad21b9
JHectorsb <152669357+JHectorsb@users.noreply.github.com>
2024-11-03 22:03:18 -0600

    Add files via upload

---
+++ boz.py
@@ -0,0 +1,16 @@
+
+x = input("Digite o valor de x: ")
+y = input("Digite o valor de y: ")
+z = input("Digite o valor de z: ")
+
+valorx =  int(x)
+valory =  int(y)
+valorz =  int(z)
+
+valor1 = 2*(valorx) + 11
+valor2 = (2*(valorz)+valory)-5
+valor3 = (valory+valorz)-valorx
+
+print(f'El valor de x es: {valor1}\n')
+print(f'El valor de y es: {valor2}\n')
+print(f'El valor de z es: {valor3}\n')
